5dc2d68ed0edc97f1f4860caa7a748f0a727fc63
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-05-30 14:39:10 +0100

    Passing the changePasswordController to its route

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const authRouter = express.Router();
-const {userSignUp, userLogin} = require('../controllers/authController')
+const {userSignUp, userLogin, changePasswordController} = require('../controllers/authController')
+const jwtMiddleware = require('../middlewares/JWT_middleware');
 
 authRouter.route('/signup').post(userSignUp);
 authRouter.route('/login').post(userLogin)
+authRouter.route('/change-password').post(jwtMiddleware, changePasswordController)
 
 module.exports = authRouter
