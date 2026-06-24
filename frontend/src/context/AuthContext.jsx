ece35a0a08a5ffc3f0165862dfdce7cfaa3485a3
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-06-24 10:09:45 +0100

    Updated Version AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,45 +1,37 @@
 import { createContext, useState, useEffect } from "react";
-
 import API from "../services/api";
 
 export const AuthContext = createContext();
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-
   const [loading, setLoading] = useState(true);
 
   const getCurrentUser = async () => {
     try {
-      const response = await API.get("/profile");
-      
-      console.log(response)
-
+      const response = await API.get("/profile", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+      });
       setUser(response.data.user);
     } catch (error) {
-      console.log(error);
-
+      console.error(error);
       localStorage.removeItem("token");
     } finally {
       setLoading(false);
     }
   };
 
-  console.log(user)
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       getCurrentUser();
-
-      setUser(user)
-    } 
-    
+    } else {
       setLoading(false);
+    }
   }, []);
 
   const logout = () => {
     localStorage.removeItem("token");
-
     setUser(null);
   };
 
@@ -47,11 +39,9 @@ function AuthProvider({ children }) {
     <AuthContext.Provider
       value={{
         user,
-
         setUser,
         getCurrentUser,
         loading,
-
         logout,
       }}
     >
